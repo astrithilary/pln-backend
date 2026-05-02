937cfe7ba0ba0df328a7389ac89b1e247edf58f8
astrithilary <astrithilary@gmail.com>
2026-05-03 01:50:02 +0800

    feat: implement authentication with Sanctum and add role to users

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PelangganController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,10 +11,19 @@ use App\Http\Controllers\Api\PelangganController;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Authentication routes (public)
+Route::post('/login', [AuthController::class, 'login']);
 
-// Endpoint Sinkronisasi untuk Flutter
-Route::post('/sync-pelanggan', [PelangganController::class, 'store']);
-Route::post('/upload-foto/{id}', [PelangganController::class, 'uploadFoto']);
+// Protected routes (require auth)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    Route::get('/user/profile', [AuthController::class, 'getUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Endpoint Sinkronisasi untuk Flutter
+    Route::post('/sync-pelanggan', [PelangganController::class, 'store']);
+    Route::post('/upload-foto/{id}', [PelangganController::class, 'uploadFoto']);
+});
