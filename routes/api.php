e5644eafe8b4955c5ab0274cf1b0bcff9d4ff984
astrithilary@gmail.com <ikadektresnajeverly78@gmail.com>
2026-05-06 01:24:06 +0800

    refactor: clean up AuthController and remove duplicate import in api routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PelangganController;
-use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
